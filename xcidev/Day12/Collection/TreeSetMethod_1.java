0c6c8117b8aa7de51d3b35375f6cd3c8311d1c97
qowl880 <qowhxk@naver.com>
2025-06-23 17:45:02 +0900

    Collection - TreeSet 실습

---
+++ xcidev/Day12/Collection/TreeSetMethod_1.java
@@ -0,0 +1,90 @@
+package Collection;
+
+import java.util.NavigableSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class TreeSetMethod_1 {
+	public static void main(String[] args) {
+		TreeSet<Integer> treeSet = new TreeSet<>();
+		for(int i =50; i>0; i-=2) {
+			treeSet.add(i);
+		}
+		
+		System.out.println(treeSet.toString());
+		// 1. first()
+		System.out.println(treeSet.first());
+		// 2. last()
+		System.out.println(treeSet.last());
+		// 3. lower()
+		System.out.println(treeSet.lower(26));
+		// 4. higher()
+		System.out.println(treeSet.higher(26));
+		// 5. floor
+		System.out.println(treeSet.floor(25));
+		System.out.println(treeSet.floor(26));
+		// 6. ceiling
+		System.out.println(treeSet.ceiling(25));
+		System.out.println(treeSet.ceiling(26));
+		System.out.println();
+		// 7. pollFirst()
+		int treeSetSize = treeSet.size();
+		System.out.println(treeSetSize);
+		for(int i=0; i<treeSetSize; i++) {
+			System.out.print(treeSet.pollFirst() + " ");
+		}
+		System.out.println();
+		System.out.println(treeSet.size());
+		
+		// 8. pollLast()
+		for(int i =50; i>0; i -=2) {
+			treeSet.add(i);
+		}
+		treeSetSize = treeSet.size();
+		for(int i=0; i<treeSetSize; i++) {
+			System.out.print(treeSet.pollLast() + " ");
+		}
+		System.out.println();
+		System.out.println(treeSet.size());
+		
+		// 9. sortedSet
+		for(int i=50; i>0; i -=2) {
+			treeSet.add(i);
+		}
+		
+		SortedSet<Integer> sSet = treeSet.headSet(20);
+		System.out.println(sSet.toString());
+		
+		// 10. NavigableSet
+		NavigableSet<Integer> nSet = treeSet.headSet(20, true);
+		System.out.println(nSet.toString());
+		nSet = treeSet.headSet(20, false);
+		System.out.println(nSet.toString());
+		
+		// 11. SortedSet
+		sSet = treeSet.tailSet(20);
+		System.out.println(sSet.toString());
+		
+		// 12. NavigableSet
+		nSet = treeSet.tailSet(20, true);
+		System.out.println(nSet.toString());
+		nSet = treeSet.tailSet(20, false);
+		System.out.println(nSet.toString());
+		
+		// 13. SortedSet subset
+		sSet = treeSet.subSet(10, 20);
+		System.out.println(sSet.toString());
+		
+		// 14. NavigableSet subset
+		nSet = treeSet.subSet(10, true, 20, false);
+		System.out.println(nSet.toString());
+		nSet = treeSet.subSet(10, false, 20, true);
+		System.out.println(nSet.toString());
+		
+		// 15. NavigableSet descendingSet
+		NavigableSet<Integer> descendingSet = treeSet.descendingSet();
+		System.out.println(descendingSet);
+		descendingSet = descendingSet.descendingSet();
+		System.out.println(descendingSet);
+	}
+}
